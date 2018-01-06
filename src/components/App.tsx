f63494948d710c5fbd1bdc9b9848ec4c6ae61d9a
Frank J. Reashore <frank.reashore@gov.bc.ca>
2018-01-05 16:08:59 -0800

    Working

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -16,12 +16,11 @@ export class App extends React.Component {
     // tslint:disable-next-line:no-console
     console.log(url);
     let dataArray: VendorData[] = this.getData(url);
-    // this.getData(url);
 
     return (
       <div>
         <h1>Axios Data Access</h1>
-        {this.formatAsTable(dataArray)}
+        {this.formatTable(dataArray)}
       </div>
     );
   }
@@ -34,7 +33,7 @@ export class App extends React.Component {
     return dataArray;
   }
 
-  private formatAsTable(dataArray: VendorData[]): JSX.Element {
+  private formatTable(dataArray: VendorData[]): JSX.Element {
     if (dataArray === undefined) {
         return (<h2>undefined</h2>);
     }
@@ -67,3 +66,60 @@ export class App extends React.Component {
     );
   }
 }
+
+// export class App extends React.Component {
+//   render() {
+//     const url: string = 'http://localhost:3001/api/vendors';
+//     // tslint:disable-next-line:no-console
+//     console.log(url);
+//     let dataArray: VendorData[] = this.getData(url);
+
+//     return (
+//       <div>
+//         <h1>Axios Data Access</h1>
+//         {this.formatTable(dataArray)}
+//       </div>
+//     );
+//   }
+
+//   private getData(url: string): VendorData[] {
+//     const dataAccess: DataAccess<VendorData> = new DataAccess<VendorData>(url);
+//     const dataArray: VendorData[] = dataAccess.getData();
+//     // tslint:disable-next-line:no-console
+//     console.log('Data = ', dataArray);
+//     return dataArray;
+//   }
+
+//   private formatTable(dataArray: VendorData[]): JSX.Element {
+//     if (dataArray === undefined) {
+//         return (<h2>undefined</h2>);
+//     }
+
+//     return (
+//       <table>
+//         <thead>
+//           <tr>
+//             <th>Vendor Id</th>
+//             <th>Name</th>
+//             <th>OCG Number</th>
+//             <th>Managing QS Area</th>
+//             <th>Primary Address</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {dataArray.map((data: VendorData, n: number) => {
+//             return (
+//               <tr key={`row ${n}`}>
+//                 <td>data.vendorId</td>
+//                 <td>data.name</td>
+//                 <td>data.ocgNumber</td>
+//                 <td>data.managingQsArea</td>
+//                 <td>data.primaryAddress</td>
+//               </tr>
+//             );
+//           })}
+//         </tbody>
+//       </table>
+//     );
+//   }
+// }
--- src/components/DataAccess.ts
+++ src/components/DataAccess.ts
@@ -16,6 +16,14 @@ export class DataAccess<T> {
         return this.data;
     }
 
+    public async getData2(): Promise<T[]> {
+        await axios.get(this.url)
+            .then(this.handleSuccess)
+            .catch(this.handleError);
+
+        return this.data;
+    }
+
     private handleSuccess(response: AxiosResponse<T[]>): void {
         console.log(response.data);
         this.data = response.data;
@@ -41,47 +49,3 @@ export class DataAccess<T> {
         console.log(error.config);
     }
 }
-
-// export class DataAccess {
-//     public data: VendorData[];
-
-//     constructor(private url: string) {
-//     }
-
-//     public getData(): VendorData[] {
-//         axios.get(this.url)
-//             .then(this.handleSuccess)
-//             .catch(this.handleError);
-
-//         return this.data;
-//     }
-
-//     // private handleSuccess<T>(response: AxiosResponse<T>): void {
-//     //   console.log('response.data = ', response.data);
-//     // }
-
-//     private handleSuccess(response: AxiosResponse<VendorData[]>): void {
-//         console.log(response.data);
-//         this.data = response.data;
-//     }
-
-//     private handleError(error: AxiosError): void {
-//         if (error.response) {
-//             // The request was made and the server responded with a status code
-//             // that falls out of the range of 2xx
-//             console.log(error.response.data);
-//             console.log(error.response.status);
-//             console.log(error.response.headers);
-//         } else if (error.request) {
-//             // The request was made but no response was received
-//             // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-//             // http.ClientRequest in node.js
-//             console.log(error.request);
-//         } else {
-//             // Something happened in setting up the request that triggered an Error
-//             console.log('Error', error.message);
-//         }
-
-//         console.log(error.config);
-//     }
-// }
